00ac4b88f310ae32f10c2aeedd542575c5487714
MuhammadUmar969 <muhammadumarazizkhan23@gmai.com>
2024-08-03 18:10:02 +0500

    add native stack

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import React from "react";
-import {
-  View,
-  Text,
-  SafeAreaView
-} from "react-native"
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TabsNavigator from './src/navigators/TabsNavigator';
+import DetailsScreen from './src/screen/DetailsScreen';
+import PaymentScreen from './src/screen/PaymentScreen';
 
-function App() {
+import React from 'react'
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello World</Text>
-        <Text>Hey Bro</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+          name='Tabs'
+          component={TabsNavigator}
+          options={{ animation: "slide_from_bottom" }}>
+
+        </Stack.Screen>
+        <Stack.Screen
+          name='Details'
+          component={DetailsScreen}
+          options={{ animation: "slide_from_bottom" }}>
+
+        </Stack.Screen>
+        <Stack.Screen
+          name='Payment'
+          component={PaymentScreen}
+          options={{ animation: "slide_from_bottom" }}>
+
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
+export default App
 
-export default App;
---
+++ src/screen/DetailsScreen.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DetailsScreen = () => {
+  return (
+    <View>
+      <Text>DetailsScreen</Text>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default DetailsScreen
+
+const styles = StyleSheet.create({})
