8d25b517e0ebe3ccbfeabe923d3f99fb7cc0f0b6
MuhammadUmar969 <muhammadumarazizkhan23@gmai.com>
2024-09-01 22:25:43 +0500

    screen updated

--- src/screen/FavouritesScreen.tsx
+++ src/screen/FavouritesScreen.tsx
@@ -1,14 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {COLORS, SPACING} from '../theme/theme';
 
 const FavouritesScreen = () => {
   return (
-    <View>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+
       <Text>FavouritesScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default FavouritesScreen
+export default FavouritesScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  ScrollViewFlex: {
+    flexGrow: 1,
+  },
+  ScrollViewInnerView: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  ItemContainer: {
+    flex: 1,
+  },
+  ListItemContainer: {
+    paddingHorizontal: SPACING.space_20,
+    gap: SPACING.space_20,
+  },
+});
